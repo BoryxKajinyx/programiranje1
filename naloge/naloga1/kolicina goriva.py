4e0235db1aadd1d56c9599161147971abf21f27e
Bor Kajin <bor.kajin@gmail.com>
2023-10-16 11:25:23 +0200

    Popravki

--- naloge/naloga1/kolicina goriva.py
+++ naloge/naloga1/kolicina goriva.py
@@ -1,9 +1,9 @@
 tovor = int(input("Vnesi količino tovora:"))
-vsota=0
+vsota = 0
 korak = 0
 while tovor > 0:
     tovor = tovor // 3 - 2
-    vsota += tovor
     korak += 1
+    vsota += max(0, tovor)
 print(korak)
 print(vsota)
